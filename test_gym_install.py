451ee12eaba67587f9fb46ca46b7db742f51d963
Pawel Wocjan <wocjan@cs.ucf.edu>
2019-09-27 17:14:24 -0400

    Remove tensorflow import

--- test_gym_install.py
+++ test_gym_install.py
@@ -1,12 +1,9 @@
 import gym
-import tensorflow as tf
 
 # env = gym.make('SpaceInvaders-v0')
 # env = gym.make('CartPole-v0')
 # env = gym.make('Taxi-v2')
 # env = gym.make('LunarLander-v2')
-
-print(tf.__version__)
 
 env = gym.make('CarRacing-v0')
 env.reset()
